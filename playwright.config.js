afcbb45034c82d76d892de27f5fc09a82f35fdca
Albert <66462943+Alpaca-AZ@users.noreply.github.com>
2026-03-17 18:15:24 +0800

    Initial commit

---
+++ playwright.config.js
@@ -0,0 +1,25 @@
+// playwright.config.js
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: 0,
+  workers: undefined,
+  reporter: 'html',
+
+  use: {
+    baseURL: 'https://www.saucedemo.com',
+    trace: 'on-first-retry',
+    screenshot: 'on',
+    video: 'retain-on-failure',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
